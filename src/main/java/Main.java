aabd4d12e0b1846b0e04c31eb335a177ac9d7bb7
Jiben <jibenapps@gmail.com>
2019-06-09 11:39:40 +0200

    Add information of game (score/life/credit)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,12 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import models.Bullet;
-import models.Game;
-import models.Spaceship;
-import models.Sprite;
+import models.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application {
 
@@ -21,6 +22,45 @@ public class Main extends Application {
     private Scene scene;
     private GraphicsContext gc = canvas.getGraphicsContext2D();
     private Spaceship spaceship;
+    private List<Alien> aliens = new ArrayList<>();
+
+    private void writeHeader() {
+        int heightText = 20;
+
+        gc.setFill(Color.WHITE);
+        gc.fillText(
+                "SCORE < 1 >" + "\n" +
+                        "0",
+                0,
+                heightText);
+
+        gc.fillText(
+                "HIGH SCORE" + "\n" +
+                "0",
+                Math.round(canvas.getWidth() / 2),
+                heightText);
+
+        gc.fillText(
+                "SCORE < 2 >" + "\n" +
+                        "0",
+                Math.round(canvas.getWidth() - 100),
+                heightText);
+    }
+
+    private void writeFooter() {
+        double heightText =  Math.round(canvas.getHeight());
+
+        gc.setFill(Color.WHITE);
+        gc.fillText(
+                getTextLives(),
+                0,
+                heightText);
+
+        gc.fillText(
+                getTextCredits(),
+                Math.round(canvas.getWidth() - 100),
+                heightText);
+    }
 
     private void addAnimatedComponents() {
         //Todo: getInstance() of Spaceship (Singleton)
@@ -35,12 +75,55 @@ public class Main extends Application {
                 15
         );
         spaceship.draw(canvas);
+
+        //Create new Alien
+        int widthAlien = 50;
+        int heightAlien = 50;
+
+        int xAlien = 0;
+        int yAlien = 150;
+
+        for(int i = 0; i < 5; i++) {
+            xAlien += widthAlien;
+            aliens.add(
+                    new Alien(
+                            xAlien,
+                            yAlien,
+                            new Sprite(Alien.imgPath, widthAlien, heightAlien),
+                            widthAlien,
+                            heightAlien,
+                            1
+                    )
+            );
+        }
+
+        for(Alien alien: aliens) {
+            alien.draw(canvas);
+        }
+
+        //AnimationTimer for Aliens
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                for(Alien alien: aliens) {
+                    if(alien.getX() >= canvas.getWidth()) {
+                        alien.setX(0);
+                        alien.setY(alien.getY() + alien.getHeight());
+                    }
+
+                    alien.moveRight(canvas);
+                }
+            }
+        }.start();
     }
 
     @Override
     public void start(Stage primaryStage) {
         //Add Canvas to Parent and AnimatedComponents
         root.getChildren().add(canvas);
+        root.setStyle("-fx-background-color: black");
+        writeHeader();
+        writeFooter();
         addAnimatedComponents();
 
         //Add Parent to Scene
@@ -90,5 +173,21 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public String getTextLives() {
+        String lTextLives;
+        int lLives = new Player().getLives();
+        if(lLives == 1 || lLives == 0) { lTextLives = lLives + " LIFE"; }
+        else { lTextLives = lLives + " LIVES"; }
+
+        return lTextLives;
+    }
+
+    public String getTextCredits() {
+        return (
+                "CREDITS " +
+                        new Player().getCredits()
+        );
     }
 }
--- src/main/java/models/Alien.java
+++ src/main/java/models/Alien.java
@@ -6,8 +6,9 @@ import lombok.Setter;
 @Getter @Setter
 public class Alien extends AnimatedComponent {
     public static final String imgPath = "images/alien.gif";
+    private static final int ySpeed = 0;
 
-    public Alien(int x, int y, Sprite sprite, int width, int height, int xSpeed, int ySpeed) {
+    public Alien(int x, int y, Sprite sprite, int width, int height, int xSpeed) {
         super(x, y, sprite, width, height, xSpeed, ySpeed);
     }
 }
