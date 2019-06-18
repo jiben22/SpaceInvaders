e80e4fed3b8d4861f48e1744dc6ffbd1dd60c682
Jiben <jibenapps@gmail.com>
2019-06-18 16:34:02 +0200

    Add SpaceCanvas to Scene

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,37 +1,21 @@
-import controllers.*;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import models.Player;
-
+import models.SpaceCanvas;
 
 public class Game extends Application{
     private Pane root = new Pane();
-    private MainController mainController = MainController.getInstance();
+    private SpaceCanvas spaceCanvas = SpaceCanvas.getInstance();
 
     @Override
     public void start(Stage primaryStage) {
         //Add SpaceCanvas to Parent
-        root.getChildren().add( mainController.canvasController.getCanvas() );
+        root.getChildren().add( spaceCanvas.getCanvas() );
         root.setStyle("-fx-background-color: black");
-
-        //Create player
-        Player player = new Player("Player1", 3, 5);
-        //Add scores and player information
-        mainController.informationBoardController.addScores(player);
-        mainController.informationBoardController.addPlayerInformation(player);
 
         //Create Scene
         Scene scene = new Scene(root);
-
-        //Create menu
-        //mainController.gameBoardController.createMenu();
-
-        //Add Animated Components
-        mainController.animatedComponentController.addAnimatedComponents();
-        //Add Event Actions for keyboard Events
-        mainController.gameBoardController.keyboardEvents(scene);
 
         primaryStage.setTitle("SpaceInvaders");
         primaryStage.setResizable(true);
